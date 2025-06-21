bf0ef8fb8bc846f804304ccdfea3eda22c2e4c4a
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2025-06-21 22:25:06 +0100

    Update serve.js

--- src/js/serve.js
+++ src/js/serve.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {
+    fileURLToPath
+} from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 1985;
 
-const staticPath = path.join(__dirname, '..', '..', 'dist', 'html');
-console.log('Serving static files from:', staticPath);
-
-app.use('/lovejs', (req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  next();
+app.use((req, res, next) => {
+    if (req.path !== '/') {
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    }
+    next();
 });
 
+const staticPath = path.join(__dirname, '..', '..', 'dist', 'html');
+console.log('Serving static files from:', staticPath);
 app.use(express.static(staticPath));
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
