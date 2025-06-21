44d5c2feae0a87dd7e3ae6b0b2b8273edb60d282
onlypuppy7 <35199523+onlypuppy7@users.noreply.github.com>
2025-06-21 11:29:37 +0100

    Csp update

--- src/js/serve.js
+++ src/js/serve.js
@@ -8,14 +8,15 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 1985;
 
-app.use((req, res, next) => {
+const staticPath = path.join(__dirname, '..', '..', 'dist', 'html');
+console.log('Serving static files from:', staticPath);
+
+app.get(['/lovejs/index.html', '/player/index.html'], (req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
 
-const staticPath = path.join(__dirname, '..', '..', 'dist', 'html');
-console.log('Serving static files from:', staticPath);
 app.use(express.static(staticPath));
 
 app.listen(PORT, () => {
